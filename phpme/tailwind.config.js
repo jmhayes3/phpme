d1b677003f4a117d408eeda752d0464af52189cb
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-08-07 23:16:30 -0500

    configure tailwind to purge unused styles in prod

--- phpme/tailwind.config.js
+++ phpme/tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+module.exports = {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
